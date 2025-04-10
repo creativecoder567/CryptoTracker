f66eaf678b04d33aa1287bf22d558faacf819777
SarathKumar N <sarathcreativethinker@gmail.com>
2025-04-10 12:01:47 +0530

    Deleted Code Reverted
    commit reverted

--- app/src/main/java/com/usbank/cryptotracker/MainActivity.kt
+++ app/src/main/java/com/usbank/cryptotracker/MainActivity.kt
@@ -4,53 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.usbank.cryptotracker.ui.screen.CryptoScreen
-import com.usbank.cryptotracker.ui.theme.CryptoTrackerTheme
-import com.usbank.cryptotracker.ui.viewmodel.CryptoViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-//    private val viewModel: CryptoViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CryptoScreen()
-       /*     CryptoTrackerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }*/
         }
     }
 }
 
-/*
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CryptoTrackerTheme {
-        Greeting("Android")
-    }
-}*/
